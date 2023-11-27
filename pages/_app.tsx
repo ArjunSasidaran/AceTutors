4a722fb35cf2dbdb3bd63567075fa5c1abc3ca24
Ayaan <ayaan@Ayaans-MacBook-Air.local>
2023-11-27 10:49:16 -0500

    Clerk implemented

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,6 @@ import { ClerkProvider } from '@clerk/nextjs'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
-        <Component {...pageProps} />
-    </ClerkProvider>
+      <Component {...pageProps} />
   )
 }
--- pages/login/student/index.tsx
+++ pages/login/student/index.tsx
@@ -3,6 +3,8 @@ import { ClerkProvider } from '@clerk/nextjs'
 
 export default function index() {
   return (
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       <div>Student login</div>
+    </ClerkProvider>
   )
 }
