ce4f21beaeb88aa480512f3c377734ee716cae7c
Arjun Sasidaran <arjunsasidaran.11@gmail.com>
2023-12-26 23:18:29 -0500

    search bar implemented

--- pages/afterauthpage/index.jsx
+++ pages/afterauthpage/index.jsx
@@ -1,19 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import styles from 'pages/afterauthpage/afterauthpage.module.css';
 
 
 const Index = () => {
+  // search value
+  const [input, setInput] = useState('');
+  const [results, setResults] = useState([]);
+  
+  const fetchData = (value) => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((response) => response.json())
+    .then((json) => {
+      const results = json.filter((item) => {
+        return (
+          value &&
+          item &&
+          item.title &&
+          item.title.toLowerCase().includes(value.toLowerCase())
+        );
+    });
+    setResults(results);  
+  });
+
+  };
+
+  const handleChange = (value) =>{
+    setInput(value)
+    fetchData(value)
+  }
+ 
   return (
-    <form className={styles.container}>
+   
+    <form className={styles.container} action = "/calender">
       <div className={styles.container2}>
-        <FaSearch />
+        <a href = "/calender"><button><FaSearch /></button></a>
         <input
           type="search"
           placeholder="Search Your Courses"
           className={styles.searchbar}
+          onChange={(e) => handleChange(e.target.value)}
+          value = {input}
         />
       </div>  
+      <div className={styles.searchResults}>
+        {results.map((result) => (
+          <a key={result.id} href= "/calender" ><button className = {styles.resultButton}>{result.title}</button></a>
+        ))}
+      </div>
     </form>
   );
 };
---
+++ pages/calender/index.jsx
@@ -0,0 +1,22 @@
+/*
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+
+function index() {
+   const [date, changeDate] = useState(new Date());
+
+   function changeValue(val) {
+      changeDate(val);
+   }
+
+   return (
+      <div>
+         <Calendar onChange = {changeValue} value = {date} />
+         <p>The selected date is - {date.toLocaleDateString()}</p>
+      </div>
+   );
+}
+export default index;
+
+*/
