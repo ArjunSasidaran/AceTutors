965fe1a562114e4b29937bc022662f260434cda8
Ayaan <mayaan0606@gmail.com>
2023-12-28 18:26:58 -0500

    Search bar and database all completed

--- pages/afterauthpagestudent/index.jsx
+++ pages/afterauthpagestudent/index.jsx
@@ -3,52 +3,69 @@ import { FaSearch } from "react-icons/fa";
 import styles from 'pages/afterauthpagestudent/afterauthpage.module.css';
 
 
+import {firestore} from "backend/server.js";
+import {collection,query,where,getDocs} from "@firebase/firestore" 
+import { debounce } from 'lodash';
+
+
 const Index = () => {
-  // search value
-  const [input, setInput] = useState('');
-  const [results, setResults] = useState([]);
-  
-  const fetchData = (value) => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-    .then((response) => response.json())
-    .then((json) => {
-      const results = json.filter((item) => {
-        return (
-          value &&
-          item &&
-          item.title &&
-          item.title.toLowerCase().includes(value.toLowerCase())
-        );
-    });
-    setResults(results);  
+// search value
+const [input, setInput] = useState('');
+const [results, setResults] = useState([]);
+
+const fetchCourseNames = async (value) => {
+  try {
+    const coursesRef = collection(firestore, 'Courses');
+    //const q = query(coursesRef, where('course name', '==', value))
+    const querySnapshot = await getDocs(coursesRef);
+   
+    const courseNames = querySnapshot.docs.map((doc) => doc.data()['course name']);
+    console.log(courseNames);
+    const results = courseNames.filter((course) => {
+      return (
+        value &&
+        course &&
+        course.toLowerCase().includes(value.toLowerCase())
+      );
   });
-
-  };
-
-  const handleChange = (value) =>{
-    setInput(value)
-    fetchData(value)
+    setResults(results);
+  } catch (error) {
+    console.error('Error fetching course names:', error.message);
   }
+};
+
+const debouncedFetchCourseNames = debounce(fetchCourseNames, 5000);
+
+const handleChange = (value) =>{
+  setInput(value)
+  debouncedFetchCourseNames(value)
+}
  
   return (
    
-    <form className={styles.container} action = "/calender">
-      <div className={styles.container2}>
-        <a href = "/calender"><button><FaSearch /></button></a>
-        <input
-          type="search"
-          placeholder="Search Your Courses"
-          className={styles.searchbar}
-          onChange={(e) => handleChange(e.target.value)}
-          value = {input}
-        />
-      </div>  
-      <div className={styles.searchResults}>
-        {results.map((result) => (
-          <a key={result.id} href= "/calender" ><button className = {styles.resultButton}>{result.title}</button></a>
-        ))}
-      </div>
-    </form>
+    <form className={styles.container} action="/calender">
+          <div className={styles.container2}>
+            <a href="/calender">
+              <button>
+                <FaSearch />
+              </button>
+            </a>
+            <input
+              type="search"
+              placeholder="Search Your Courses"
+              className={styles.searchbar}
+              onChange={(e) => handleChange(e.target.value)}
+              value={input}
+            />
+          </div>
+          <div className={styles.searchResults}>
+            {results.map((courseName, index) => (
+              <a key={index} href="/calender">
+                <button className={styles.resultButton}>{courseName}</button>
+              </a>
+            ))}
+          </div>
+        </form>
   );
 };
 
