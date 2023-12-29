bfbb825eefb058a6d0df5ce4b2215b80bedaf163
Ayaan <mayaan0606@gmail.com>
2023-12-28 23:35:33 -0500

    tutor form done

--- pages/calender/index.jsx
+++ pages/calender/index.jsx
@@ -1,23 +1,36 @@
-// Import React and CSS file
+// JSX file (e.g., index.jsx)
 import React from "react";
-import './calender.module.css';
+import styles from "./calender.module.css";
 
-// Create a functional component
 const Index = () => {
   return (
-    <div className="container">
-      <ul>
-        <li>
-          <div className="bottom">Heading</div>
-        </li>
-        <li>
-          <div className="bottom">Heading</div>
-        </li>
-        {/* Add more li elements as needed */}
-      </ul>
+    <div className={styles.create}>
+      <h2 className={styles.createh2}>Become a Tutor for CourseName</h2>
+
+      <div className={styles.formContainer}>
+        <form>
+          <div className={styles.fieldContainer}>
+            <label className={styles.createlabel}>Name:</label>
+            <input type="text" className={styles.createinput} required />
+          </div>
+
+          <div className={styles.fieldContainer}>
+            <label className={styles.createlabel}>Bio:</label>
+            <textarea className={styles.createtextarea} required />
+          </div>
+
+          <div className={styles.fieldContainer}>
+            <label className={styles.createlabel}>Contact Details:</label>
+            <input type="text" className={styles.createinput} required />
+          </div>
+
+          <button type="submit" className={styles.createbutton}>
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
 
-// Export the component
 export default Index;
