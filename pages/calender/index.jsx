d837092de8b0c5a84b03dede1057c3e52b41a928
Justin Cao <caojustin04@gmail.com>
2023-12-30 15:22:37 -0500

    Added database connection to tutor form

--- pages/calender/index.jsx
+++ pages/calender/index.jsx
@@ -1,27 +1,52 @@
 // JSX file (e.g., index.jsx)
-import React from "react";
+import React, {useRef} from "react";
 import styles from "./calender.module.css";
+import {firestore} from "backend/server.js";
+import {addDoc, collection} from "@firebase/firestore" 
 
 const Index = () => {
+
+  const ref = collection(firestore, "TutorCourse")
+  const bio = useRef();
+  const avail = useRef();
+  const contact = useRef();
+
+  const handleSave = async(e) => {
+    e.preventDefault();
+    console.log(bio.current.value);
+    console.log(avail.current.value);
+    console.log(contact.current.value);
+    let data = {
+      bio: bio.current.value,
+      avail: avail.current.value,
+      contact: contact.current.value,
+    }
+    try {
+      await addDoc(ref, data);
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
   return (
     <div className={styles.create}>
       <h2 className={styles.createh2}>Become a Tutor for CourseName</h2>
 
       <div className={styles.formContainer}>
-        <form>
+        <form onSubmit={handleSave}> {/* Moved onSubmit to the form element */}
           <div className={styles.fieldContainer}>
             <label className={styles.createlabel}>Bio:</label>
-            <input type="text" className={styles.createinput} required />
+            <input type="text" ref={bio} className={styles.createinput} required />
           </div>
 
           <div className={styles.fieldContainer}>
             <label className={styles.createlabel}>Availability:</label>
-            <textarea className={styles.createtextarea} required />
+            <textarea ref={avail} className={styles.createtextarea} required />
           </div>
 
           <div className={styles.fieldContainer}>
             <label className={styles.createlabel}>Contact Details:</label>
-            <input type="text" className={styles.createinput} required />
+            <input type="text" ref={contact} className={styles.createinput} required />
           </div>
 
           <button type="submit" className={styles.createbutton}>
