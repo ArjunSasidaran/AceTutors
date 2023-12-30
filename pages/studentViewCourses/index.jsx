48dbfa11b0aea32fec821eb9ac6708fcdfc8537f
Ayaan <mayaan0606@gmail.com>
2023-12-30 00:52:44 -0500

    Available Tutors

--- pages/studentViewCourses/index.jsx
+++ pages/studentViewCourses/index.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
 
-const index = () => {
+const Index = () => {
   return (
-    <div>Hey Miyute!</div>
-  )
+    <div className="max-w-6xl m-auto flex items-start justify-center" style={{ height: "40vh" }}>
+      {/* Dashboard at the left */}
+      <div className="p-4">
+        <h1 className="font-bold text-4xl text-cyan-950 fade-in">Available Tutors</h1>
+      </div>
+    </div>
+  );
 }
 
-export default index
+export default Index;
