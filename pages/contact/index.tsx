d0b752707486ac8a5f85e334e43488985ef8fde0
Ayaan <mayaan0606@gmail.com>
2024-01-12 18:35:14 -0500

    email added

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import styles from 'pages/contact/contact.module.css'; // Import the CSS file
+import React from "react";
+import styles from "pages/contact/contact.module.css"; // Import the CSS file
 
 const Index = () => {
   return (
     <div className={styles.container}>
       <p>
-        Send us an email at{' '}
-        <strong className={styles.email}>contact@example.com</strong>, and we will get back to you in a couple of hours.
+        Send us an email at{" "}
+        <strong className={styles.email}>acetutorsorg@gmail.com</strong>, and we
+        will get back to you in a couple of hours.
       </p>
     </div>
   );
